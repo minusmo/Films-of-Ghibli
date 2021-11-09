9d4ae1786cf00e64f9f5432380ba44d0591a672c
hojooneum <bldolphin96@gmail.com>
2021-11-10 01:35:28 +0900

    [UPDATE] changed to OOP pattern

--- app.js
+++ app.js
@@ -1,167 +1,174 @@
-const root = document.getElementById("root");
+const GHIBLI_API = "https://ghibliapi.herokuapp.com/films";
 
-const makeHeroFrame = (_) => {
-  const heroFrame = document.createElement("div");
-  heroFrame.classList.add("heroFrame");
-  return heroFrame;
-};
+class Ghibli {
+  constructor() {
+    this.root = document.getElementById("root");
+    this.buildPage();
+    document.addEventListener("scroll", this.onScroll);
+  }
 
-function makeFilmFrame() {
-  let filmFrame = document.createElement("div");
-  filmFrame.className = "filmFrame";
-  return filmFrame;
-}
+  makeHeroFrame() {
+    const heroFrame = document.createElement("div");
+    heroFrame.classList.add("heroFrame");
+    return heroFrame;
+  }
 
-const hideModal = (e) => {
-  e.preventDefault();
-  const modal = e.target.parentElement.parentElement;
-  modal.classList.remove("showing");
-  modal.style.animation = "fadeOut 500ms forwards";
-};
+  makeFilmFrame() {
+    let filmFrame = document.createElement("div");
+    filmFrame.className = "filmFrame";
+    return filmFrame;
+  }
 
-const showModal = (e) => {
-  e.preventDefault();
-  const modal = e.target.nextElementSibling;
-  modal.classList.add("showing");
-  modal.style.animation = "fadeIn 500ms forwards";
-};
+  hideModal(e) {
+    e.preventDefault();
+    const modal = e.target.parentElement.parentElement;
+    modal.classList.remove("showing");
+    modal.style.animation = "fadeOut 500ms forwards";
+  }
 
-function makeList(film) {
-  let titleList = document.createElement("ul");
-  titleList.classList.add("titleList");
+  showModal(e) {
+    e.preventDefault();
+    const modal = e.target.nextElementSibling;
+    modal.classList.add("showing");
+    modal.style.animation = "fadeIn 500ms forwards";
+  }
 
-  let modal = document.createElement("div");
-  modal.classList.add("infoModal");
+  makeList(film) {
+    let titleList = document.createElement("ul");
+    titleList.classList.add("titleList");
 
-  let closeButton = document.createElement("button");
-  let closeImg = document.createElement("img");
-  closeImg.src = "./images/cross.svg";
+    let modal = document.createElement("div");
+    modal.classList.add("infoModal");
 
-  closeButton.classList.add("closeButton");
-  closeImg.addEventListener("click", hideModal);
-  closeButton.appendChild(closeImg);
-  modal.appendChild(closeButton);
+    let closeButton = document.createElement("button");
+    let closeImg = document.createElement("img");
+    closeImg.src = "./images/cross.svg";
 
-  let infoList = document.createElement("ul");
-  infoList.classList.add("infoList");
+    closeButton.classList.add("closeButton");
+    closeButton.appendChild(closeImg);
+    closeImg.addEventListener("click", this.hideModal);
+    modal.appendChild(closeButton);
 
-  for (let [key, value] of Object.entries(film)) {
-    if (
-      key === "id" ||
-      key === "people" ||
-      key === "image" ||
-      key === "movie_banner" ||
-      key === "species" ||
-      key === "locations" ||
-      key === "vehicles" ||
-      key === "url"
-    ) {
-      continue;
-    }
+    let infoList = document.createElement("ul");
+    infoList.classList.add("infoList");
 
-    let listHead = document.createElement("li");
-    listHead.classList.add("listHead");
-    let listContent = document.createElement("li");
-    listContent.classList.add("listContent");
-    const uppercaseKey = key.toUpperCase();
-    listHead.textContent = `${uppercaseKey}:`;
+    for (let [key, value] of Object.entries(film)) {
+      if (
+        key === "id" ||
+        key === "people" ||
+        key === "image" ||
+        key === "movie_banner" ||
+        key === "species" ||
+        key === "locations" ||
+        key === "vehicles" ||
+        key === "url"
+      ) {
+        continue;
+      }
 
-    if (key === "title" || key === "original_title") {
-      listContent.textContent = `${value}`;
+      let listHead = document.createElement("li");
+      listHead.classList.add("listHead");
+      let listContent = document.createElement("li");
+      listContent.classList.add("listContent");
+      const uppercaseKey = key.toUpperCase();
+      listHead.textContent = `${uppercaseKey}:`;
 
-      titleList.appendChild(listHead);
-      titleList.appendChild(listContent);
-    } else {
-      if (key === "description") {
-        listHead.className = "description";
-        let slicedDescription = value.slice(0, 150);
-        slicedDescription += "...";
-        listContent.textContent = slicedDescription;
-      } else {
+      if (key === "title" || key === "original_title") {
         listContent.textContent = `${value}`;
+        titleList.appendChild(listHead);
+        titleList.appendChild(listContent);
+      } else {
+        if (key === "description") {
+          listHead.className = "description";
+          let slicedDescription = value.slice(0, 150);
+          slicedDescription += "...";
+          listContent.textContent = slicedDescription;
+        } else {
+          listContent.textContent = `${value}`;
+        }
+        infoList.appendChild(listHead);
+        infoList.appendChild(listContent);
       }
-
-      infoList.appendChild(listHead);
-      infoList.appendChild(listContent);
     }
+
+    let moreInfo = document.createElement("li");
+    moreInfo.classList.add("moreInfo");
+    moreInfo.textContent = "more info";
+    moreInfo.addEventListener("click", this.showModal);
+
+    titleList.appendChild(moreInfo);
+    titleList.appendChild(modal);
+    modal.appendChild(infoList);
+
+    return titleList;
   }
 
-  let moreInfo = document.createElement("li");
-  moreInfo.classList.add("moreInfo");
-  moreInfo.textContent = "more info";
-  moreInfo.addEventListener("click", showModal);
+  makeCatalog() {
+    const catalog = document.createElement("div");
+    catalog.className = "catalog";
+    return catalog;
+  }
 
-  titleList.appendChild(moreInfo);
-  titleList.appendChild(modal);
-  modal.appendChild(infoList);
+  buildPage() {
+    fetch(GHIBLI_API)
+      .then((res) => {
+        return res.json();
+      })
+      .then((filmdata) => {
+        filmdata.forEach((film, index) => {
+          switch (film.title) {
+            case "Grave of the Fireflies":
+              break;
+            case "Only Yesterday":
+              break;
+            case "Pom Poko":
+              break;
+            case "My Neighbors the Yamadas":
+              break;
+            default:
+              let heroFrame = this.makeHeroFrame();
+              heroFrame.id = index;
 
-  return titleList;
-}
+              let imgFrame = document.createElement("div");
+              let img = document.createElement("img");
+              img.setAttribute("id", `${film.id}`);
 
-function makeCatalog() {
-  const catalog = document.createElement("div");
-  catalog.className = "catalog";
-  return catalog;
-}
+              let titleList = this.makeList(film);
+              let catalog = this.makeCatalog();
 
-fetch("https://ghibliapi.herokuapp.com/films")
-  .then((res) => {
-    return res.json();
-  })
-  .then((filmdata) => {
-    filmdata.forEach((film, index) => {
-      switch (film.title) {
-        case "Grave of the Fireflies":
-          break;
-        case "Only Yesterday":
-          break;
-        case "Pom Poko":
-          break;
-        case "My Neighbors the Yamadas":
-          break;
-        default:
-          let heroFrame = makeHeroFrame();
-          heroFrame.id = index;
+              imgFrame.classList.add("filmFrame");
+              imgFrame.appendChild(img);
+              catalog.appendChild(titleList);
+              heroFrame.appendChild(catalog);
+              heroFrame.appendChild(imgFrame);
 
-          let imgFrame = document.createElement("div");
-          let img = document.createElement("img");
-          img.setAttribute("id", `${film.id}`);
+              img.src = `${film.image}`;
 
-          let titleList = makeList(film);
-          let catalog = makeCatalog();
+              this.root.appendChild(heroFrame);
+          }
+        });
+      })
+      .catch((err) => console.warn(err));
+  }
 
-          imgFrame.classList.add("filmFrame");
-          imgFrame.appendChild(img);
-          catalog.appendChild(titleList);
-          heroFrame.appendChild(catalog);
-          heroFrame.appendChild(imgFrame);
+  onScroll(e) {
+    const scrollTop = document.scrollingElement.scrollTop;
+    const pageBottomPos = scrollTop + window.innerHeight;
+    const frames = document.getElementsByClassName("heroFrame");
 
-          img.src = `${film.image}`;
+    for (let i = 0; i < frames.length; i++) {
+      const aFrame = frames[i];
+      const topPos = aFrame.offsetTop;
 
-          root.appendChild(heroFrame);
+      if (topPos < pageBottomPos) {
+        aFrame.classList.add("visible");
+        aFrame.firstElementChild.classList.add("visible");
+      } else {
+        aFrame.classList.remove("visible");
+        aFrame.firstElementChild.classList.remove("visible");
       }
-      return root;
-    });
-  })
-  .catch((err) => console.warn(err));
-
-const onScroll = (e) => {
-  const scrollTop = document.scrollingElement.scrollTop;
-  const pageBottomPos = scrollTop + window.innerHeight;
-  const frames = document.getElementsByClassName("heroFrame");
-
-  for (i = 0; i < frames.length; i++) {
-    const aFrame = frames[i];
-    const topPos = aFrame.offsetTop;
-
-    if (topPos < pageBottomPos) {
-      aFrame.classList.add("visible");
-      aFrame.firstElementChild.classList.add("visible");
-    } else {
-      aFrame.classList.remove("visible");
-      aFrame.firstElementChild.classList.remove("visible");
     }
   }
-};
+}
 
-document.addEventListener("scroll", onScroll);
+window.ghibli = new Ghibli();
