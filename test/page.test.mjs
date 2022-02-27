27b9e507b66270f78a4968c17444fa274f619c2f
hojooneum <bldolphin96@gmail.com>
2022-02-27 22:28:26 +0900

    [TEST] header instance test passed

---
+++ test/page.test.mjs
@@ -0,0 +1,52 @@
+"use strict";
+import { suite, test } from "mocha";
+import chai from "chai";
+let expect = chai.expect;
+
+class Page {
+  constructor(header, main, footer) {
+    this.header = header ? header : null;
+    this.main = main ? main : null;
+    this.footer = footer ? footer : null;
+    
+  }
+
+  addHeader() {
+
+  }
+
+  addMain() {
+
+  }
+
+  addFooter() {
+
+  }
+}
+
+class Header {
+  constructor() {
+
+  }
+}
+
+suite("test Page instance property", function () {
+  test("Page exists", function () {
+    expect(new Page()).is.not.undefined;
+  });
+  test("Page instance has header", function () {
+    expect(new Page("header")).to.has.property("header");
+  });
+  test("Page instance has main", function () {
+    expect(new Page("header", "main")).to.has.property("main");
+  });
+  test("Page instance has footer", function () {
+    expect(new Page("header", "main", "footer")).to.has.property("footer");
+  });
+});
+
+suite("test Header instance", function () {
+  test("Header exists", function () {
+    expect(new Header()).is.not.undefined;
+  });
+});
