0bd9d9714c9afcfdf0d7ca2b11a46f0a62bc8c96
hojooneum <bldolphin96@gmail.com>
2022-08-13 14:43:13 +0900

    Add Divider view

---
+++ src/javascript/modules/Views/StaticView/Divider.mjs
@@ -0,0 +1,27 @@
+"use strict";
+
+const style = `
+    <style>
+        :host {
+            position: absolute;
+            display: block;
+            background-color: black;
+            width: 2px;
+            height: 90%;
+            top: 5%;
+            left: 50%;
+        }
+    </style>
+`;
+
+export class Divider extends HTMLElement {
+    #shadowRoot;
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: "open"});
+        shadowRoot.innerHTML = style;
+        this.#shadowRoot = shadowRoot;
+    }
+}
+
+customElements.define("divider-view", Divider);
